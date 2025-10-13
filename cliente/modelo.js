44d8ec53c21b9e7bd9e51a2e4be91b988a1fc836
EstherCarrilero <esther.carrileropolidura@gmail.com>
2025-10-13 08:45:00 +0200

    Implementeción de lógica y pruebas

---
+++ cliente/modelo.js
@@ -0,0 +1,26 @@
+function Sistema(){ 
+    this.usuarios={}; 
+    this.agregarUsuario=function(nick){ 
+        this.usuarios[nick]=new Usuario(nick); 
+    } 
+
+    this.obtenerUsuarios=function(){
+        return this.usuarios;
+    }
+
+    this.usuarioActivo=function(nick){
+        return (this.usuarios[nick]) ? true : false;
+    }
+
+    this.eliminarUsuario=function(nick){
+        delete this.usuarios[nick];
+    }
+
+    this.numeroUsuarios=function(){
+        return Object.keys(this.usuarios).length;
+    }
+}
+
+function Usuario(nick){ 
+    this.nick=nick; 
+} 
